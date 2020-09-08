bad450d1d2b64d2d889f86081947d10a5a1b9587
Malgorzata Kot <malgorzata.kot@jobcloud.ch>
2020-09-08 19:07:28 +0200

    added default image and localstorage

--- src/client/index.js
+++ src/client/index.js
@@ -1,5 +1,9 @@
 import { getGeoName } from "./js/apiHandlers";
-import { handleSubmit } from "./js/formHandler";
+import {
+  handleSubmit,
+  updateLastTrips,
+  getLocalStorage,
+} from "./js/formHandler";
 import "./styles/resets.scss";
 import "./styles/base.scss";
 import "./styles/footer.scss";
@@ -7,6 +11,15 @@ import "./styles/form.scss";
 import "./styles/header.scss";
 
 const submitButton = document.getElementById("submit");
+const dateField = document.getElementById("date");
+const storedLocations = getLocalStorage();
+
+if (storedLocations != null) {
+  updateLastTrips(getLocalStorage());
+}
+
+dateField.min = new Date().toISOString().substring(0, 10);
+dateField.value = new Date().toISOString().substring(0, 10);
 
 submitButton.addEventListener("click", () => Client.handleSubmit(event));
 
--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -15,7 +15,11 @@ async function handleSubmit(event) {
   const datesDiff = Math.ceil((dateInput - todaysDate) / (1000 * 3600 * 24));
   const coordinates = await getGeoData(formText);
 
-  console.log(coordinates);
+  if (coordinates == null) {
+    setDefaults();
+    alert("Please enter a valid location");
+    return;
+  }
 
   const weatherPromise = getWeather(
     dateInput,
@@ -24,14 +28,13 @@ async function handleSubmit(event) {
     coordinates.lon
   );
   const imageResponsePromise = getImage(formText);
-  // console.log(imageResponse["hits"][0]["webformatURL"]);
 
   let responseObject = await Promise.all([
     imageResponsePromise,
     weatherPromise,
   ]).then(function (values) {
     return {
-      image: values[0]["hits"][0]["webformatURL"],
+      image: values[0],
       weather: values[1],
     };
   });
@@ -41,11 +44,19 @@ async function handleSubmit(event) {
     datesDiff
   );
 
+  responseObject["image"] = processImage(responseObject["image"]);
+
   updateUI(responseObject);
+  addToLocalStorage(formText);
+  updateLastTrips(getLocalStorage());
 }
 
 async function getGeoData(location) {
   const sentimentResponse = await getGeoName(location);
+
+  if (sentimentResponse["geonames"].length === 0) {
+    return;
+  }
 
   return {
     lat: sentimentResponse["geonames"][0]["lat"],
@@ -54,22 +65,17 @@ async function getGeoData(location) {
 }
 
 function getWeather(date, todaysDate, lat, lon) {
-  // let weatherResponse = {};
   let responseType = "forecast/daily";
 
   if (date.setHours(0, 0, 0, 0) == todaysDate.setHours(0, 0, 0, 0)) {
     responseType = "current";
   }
-  // weatherResponse = weatherResponse["data"][0];
-
-  console.log(lat, lon);
 
   return getWeatherData(responseType, lat, lon);
 }
 
 function processWeatherData(data, datesDiff) {
   let weatherResponse = {};
-  console.log(datesDiff);
   if (datesDiff == 0) {
     weatherResponse = data["data"][0];
   } else if (datesDiff <= 16) {
@@ -84,14 +90,28 @@ function processWeatherData(data, datesDiff) {
   return { temperature: temperature, description: description };
 }
 
+function processImage(dataObject) {
+  if (dataObject["hits"].length > 0) {
+    dataObject = dataObject["hits"][0]["webformatURL"];
+  } else {
+    dataObject = "src/client/img/unknown_location.png";
+  }
+
+  return dataObject;
+}
+
 function updateUI(dataObject) {
   let temp = document.getElementById("temperature");
   let weather = document.getElementById("weather");
   let picture = document.getElementById("picture");
+  let pictureSection = document.getElementById("picture_section");
+  let resultSection = document.getElementById("results_section");
 
+  resultSection.style.display = "block";
   temp.innerText = dataObject["weather"]["temperature"];
   weather.innerText = dataObject["weather"]["description"];
   picture.style.backgroundImage = `url(${dataObject["image"]})`;
+  pictureSection.style.display = "block";
 }
 
 /**
@@ -99,9 +119,49 @@ function updateUI(dataObject) {
  */
 
 function setDefaults() {
-  document.getElementById("subjectivity").innerHTML = "";
-  document.getElementById("confidence").innerHTML = "";
-  document.getElementById("irony").innerHTML = "";
+  let picture = document.getElementById("picture");
+  let pictureSection = document.getElementById("picture_section");
+  let resultSection = document.getElementById("results_section");
+
+  resultSection.style.display = "none";
+  picture.style.backgroundImage = "";
+  pictureSection.style.display = "none";
+  document.getElementById("temperature").innerHTML = "";
+  document.getElementById("weather").innerHTML = "";
 }
 
-export { handleSubmit };
+function addToLocalStorage(location) {
+  let values = [];
+  if (localStorage["geoname"]) {
+    values = getLocalStorage();
+  }
+
+  if (values.indexOf(location) == -1) {
+    values.unshift(location);
+    if (values.length > 5) {
+      values.pop();
+    }
+    localStorage["geoname"] = JSON.stringify(values);
+  }
+}
+
+function getLocalStorage() {
+  return JSON.parse(localStorage.getItem("geoname"));
+}
+
+function updateLastTrips(list) {
+  let newRow = {};
+  let newBody = document.createElement("tbody");
+  let resultsTable = document
+    .getElementById("trips_table")
+    .getElementsByTagName("tbody")[0];
+
+  for (const val of list) {
+    newRow = newBody.insertRow(newBody.rows.length);
+    newRow.innerHTML = "<td>" + val + "</td>";
+  }
+
+  resultsTable.parentNode.replaceChild(newBody, resultsTable);
+}
+
+export { handleSubmit, updateLastTrips, getLocalStorage };
